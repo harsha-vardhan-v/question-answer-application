3b3a2b474238f6a49d4be9eff72eb4e789614b4b
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2022-11-05 20:35:48 +0530

    Created Question and Answer models

--- qa-backend-api/src/main/java/com/example/qa/model/Question.java
+++ qa-backend-api/src/main/java/com/example/qa/model/Question.java
@@ -1,4 +1,34 @@
 package com.example.qa.model;
 
-public class Question {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Question extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "question_generator")
+    @SequenceGenerator(
+            name = "question_generator",
+            sequenceName = "question_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Column(columnDefinition = "text")
+    private String description;
 }
